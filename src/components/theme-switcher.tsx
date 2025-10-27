e8ff4398cd097555d5c4c789159cea03e96a9c6d
Niyondiko Joffre <niyondikojoffreasjubu@gmail.com>
2025-10-27 21:58:33 +0000

    tu as supprime button de switch theme svp

--- src/components/theme-switcher.tsx
+++ src/components/theme-switcher.tsx
@@ -9,11 +9,8 @@ import { Button } from "@/components/ui/button"
 export function ThemeSwitcher() {
   const { theme, setTheme } = useTheme()
 
-  // This app is dark-theme only based on the new design.
-  // This component will be hidden, but kept in case of future changes.
-
   return (
-    <div className="hidden">
+    <div>
       <Button variant="ghost" size="icon" onClick={() => setTheme(theme === "light" ? "dark" : "light")}>
         <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
         <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
