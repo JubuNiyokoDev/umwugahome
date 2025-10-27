e03e58b42d10a24ac1c6481ced376c5199276900
Niyondiko Joffre <niyondikojoffreasjubu@gmail.com>
2025-10-27 08:51:20 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8404575895-b3416",
+  "appId": "1:977404918447:web:de25351d0c5fcd91a1725a",
+  "apiKey": "AIzaSyAhQOrv3bEgHHV9vnO2ugosagwrQWmyE50",
+  "authDomain": "studio-8404575895-b3416.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "977404918447"
+};
